b66c2d4b60465c3367487469edde91f4d7161b90
yuviban <yuvrajsinghch4@gmail.com>
2022-04-09 10:49:25 +0530

    squade_dbconnect_v2

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017";
+
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("connected sucesfully");
+    })
+}
+module.exports = connectToMongo;
